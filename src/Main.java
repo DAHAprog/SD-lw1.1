8e6c1e5d1c5ae7f48b5978f4f083a219fb192ccd
Remez Pavel <github.aidecay@gmail.com>
2020-10-11 14:44:26 +0300

    Add LambdaManager class

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,17 @@
 public class Main {
 
-    public static void main(String[] args) {
+    public static class LambdaManager {
 
+        public static void main(String[] args) {
+
+            Operationable operation;
+            operation = (x,y)->x+y;
+
+            int result = operation.calculate(4578678, 58757);
+            System.out.println(result);
+        }
+    }
+    interface Operationable{
+        int calculate(int x, int y);
     }
 }
